dca4a53b2e400e02fd59f65474b46083c1f5a432
David Castello <davidcastellobatetgmail.com>
2022-09-10 20:04:22 +0200

    humberger responsive + mediaqueries

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('conectat');
+const hamburgerButton = document.getElementById("hamburger");
+
+const navList = document.getElementById("nav-list");
+
+
+function toogleButton(){
+  navList.classList.toggle("show");
+}
+
+hamburgerButton.addEventListener("click", toogleButton)
